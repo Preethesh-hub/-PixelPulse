f783ad81db30922b1bc18371d0625a43bcdc4755
preetheshsa26300450-dev. <preetheshsa.26300450@kalvium.community>
2026-09-24 15:13:07 +0530

    Add scaffold and UI

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript functionality will be implemented in Part 2.
+// Currently listening for search form submission to prevent default reload.
+
+document.getElementById('search-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('Search triggered, but no behaviour implemented yet!');
+});
